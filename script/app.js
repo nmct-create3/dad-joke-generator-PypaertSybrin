7e8d78f1cc3bf2deef6e5a670216784500259f8c
PypaertSybrin <sybrin.pypaert@student.howest.be>
2023-10-11 10:48:39 +0200

    Generate jokes

--- script/app.js
+++ script/app.js
@@ -1,5 +1,44 @@
+const jokeContent = document.querySelector('.js-joke-content');
+const jokeButton = document.querySelector('.js-joke-button');
+const jokeLoader = document.querySelector('.js-joke-loader');
+
+let loadingDelayed;
+
+const fetchJoke = () =>{
+    return fetch('https://icanhazdadjoke.com/', {
+        headers: {
+            Accept: 'application/json',
+        },
+    }).then((response) => response.json())
+}
+
+const showLoader = () =>{
+    loadingDelayed = setTimeout(() => {
+        jokeLoader.classList.remove('u-hidden');
+    }, 500)
+    jokeContent.classList.add('u-hidden');
+}
+const removeLoader = () =>{
+    if(loadingDelayed) {
+        clearTimeout(loadingDelayed);
+        loadingDelayed = null;
+    }
+
+    jokeLoader.classList.add('u-hidden');
+    jokeContent.classList.remove('u-hidden');
+}
+
+const generateJoke = async () => {
+    showLoader();
+    const {joke} = await fetchJoke();
+    jokeContent.innerHTML = joke;
+    removeLoader();
+}
+
 const init = function () {
     console.log('script loaded');
+    // generateJoke();
+    jokeButton.addEventListener('click', generateJoke);
 }
 
 init();
